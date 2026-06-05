28359a81809fec8927c5eda7efc635ddc1d3f9a8
jacekm91fidzinski-cmd <jacekm91fidzinski@gmail.com>
2026-06-05 16:03:42 +0200

    hibernate-configuration-hw

---
+++ src/main/java/mate/academy/util/HibernateUtil.java
@@ -0,0 +1,27 @@
+package mate.academy.util;
+
+import mate.academy.model.Movie;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory = initSessionFactory();
+
+    private HibernateUtil() {
+    }
+
+    private static SessionFactory initSessionFactory() {
+        try {
+            return new Configuration()
+                    .configure("hibernate.cfg.xml")
+                    .addAnnotatedClass(Movie.class)
+                    .buildSessionFactory();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't create SessionFactory", e);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
